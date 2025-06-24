ccb648a09fd4b92fc6c9623f3952d97ade63de35
JohnSang16 <johnsang1970@gmail.com>
2025-06-24 18:12:56 -0400

    completed reset button with new grid creation successfull

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 const body = document.querySelector("body");
 const btn = document.createElement("button");
-const container = document.createElement("div");
+let container = document.createElement("div");
 container.classList.add("container");
 container.style.cssText = "height: 544px; width: 544px; display:flex; flex-wrap: wrap; border: 15px solid red";
 btn.textContent = "reset"
@@ -25,12 +25,33 @@ btn.addEventListener("click", function(){
     body.removeChild(container);
     const horiz = prompt("Enter the num of squares for horizontal side ");
     const vert = prompt("Enter the num of squares for vertical size ");
-    const newContainer = document.createElement("div");
+    container = document.createElement("div");
+
+
+    container.classList.add("newContainer");
+    container.style.cssText = "height: 544px; width: 544px; display:flex; flex-wrap: wrap; border: 15px solid red";
+    body.appendChild(container);
+
+    const squareWidth = 544/horiz;
+    const squareHeight= 544/vert;
+
+
+    for(let i = 0; i< horiz; i++){
+        for(let j = 0; j < vert; j++){
+            const newSquare = document.createElement("div");
+            newSquare.classList.add("newSquare");
+
+
+            newSquare.addEventListener("mouseover", function(){
+            newSquare.style.backgroundColor = "black";
+});
+
+            newSquare.style.cssText = `height: ${squareHeight-2}px; width: ${squareWidth-2}px; border:1px solid black;background-color:white; `
+            container.appendChild(newSquare);
 
     
-    newContainer.classList.add("newContainer");
-    newContainer.style.cssText = "height: 544px; width: 544px; display:flex; flex-wrap: wrap; border: 15px solid red";
-    body.appendChild(newContainer);
+        }
+    }
 
 })
 
