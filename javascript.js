57c9d19bc2999ba6b08127fba8f5320e3092aa98
JohnSang16 <johnsang1970@gmail.com>
2025-06-24 20:16:56 -0400

    toggle for the paint created

--- javascript.js
+++ javascript.js
@@ -5,6 +5,47 @@ let container = document.createElement("div");
 container.classList.add("container");
 container.style.cssText = "height: 544px; width: 544px; display:flex; flex-wrap: wrap; border: 15px solid red";
 btn.textContent = "reset"
+const redbtn = document.createElement("button");
+redbtn.textContent = "red";
+const greenbtn = document.createElement("button");
+greenbtn.textContent = "green";
+const blackbtn = document.createElement("button");
+blackbtn.textContent = "black";
+const whitebtn = document.createElement("button");
+whitebtn.textContent = "white";
+let currentColor = "black";
+
+function enablePainting(container) {
+  container.addEventListener("mousedown", () => {
+    painting = true;
+  });
+
+  container.addEventListener("mouseup", () => {
+    painting = false;
+  });
+}
+enablePainting(container);
+
+
+
+redbtn.addEventListener("click", function(){
+    currentColor = "red";
+})
+
+greenbtn.addEventListener("click", function(){
+    currentColor = "green";
+})
+
+
+blackbtn.addEventListener("click", function(){
+    currentColor = "black";
+})
+
+
+whitebtn.addEventListener("click", function(){
+    currentColor = "white";
+})
+
 
 for (let i = 0; i< 16; i++){
     for (let j = 0; j< 16; j++){
@@ -12,7 +53,9 @@ for (let i = 0; i< 16; i++){
     square.classList.add("square");
 
     square.addEventListener("mouseover", function(){
-    square.style.backgroundColor = "black";
+    if (painting){
+        square.style.backgroundColor = currentColor;
+    }
 });
 
 
@@ -22,7 +65,9 @@ for (let i = 0; i< 16; i++){
 }
 
 btn.addEventListener("click", function(){
-    body.removeChild(container);
+ body.removeChild(container);
+
+
     let horiz = prompt("Enter the num of squares for horizontal side: ");
     while(horiz>100 || horiz <= 0 || isNaN(horiz)){
         horiz = prompt("Please enter a valid num from 1-100 for horizontal side:")
@@ -38,6 +83,9 @@ btn.addEventListener("click", function(){
     container.style.cssText = "height: 544px; width: 544px; display:flex; flex-wrap: wrap; border: 15px solid red";
     body.appendChild(container);
 
+    enablePainting(container);
+
+
     const squareWidth = 544/horiz;
     const squareHeight= 544/vert;
 
@@ -47,10 +95,13 @@ btn.addEventListener("click", function(){
             const newSquare = document.createElement("div");
             newSquare.classList.add("newSquare");
 
-
+            
             newSquare.addEventListener("mouseover", function(){
-            newSquare.style.backgroundColor = "black";
-});
+            if(painting){newSquare.style.backgroundColor = currentColor; 
+            }
+
+    });
+
 
             newSquare.style.cssText = `height: ${squareHeight-2}px; width: ${squareWidth-2}px; border:1px solid black;background-color:white; `
             container.appendChild(newSquare);
@@ -59,9 +110,14 @@ btn.addEventListener("click", function(){
         }
     }
 
+   
 })
 
 
 
+body.appendChild(redbtn);
+body.appendChild(greenbtn);
+body.appendChild(blackbtn);
+body.appendChild(whitebtn);
 body.appendChild(btn);
 body.appendChild(container);
